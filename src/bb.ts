b19bb70396a0a10a032c4ae21f11c0d46c05e53a
RepComm <rccommandotac@gmail.com>
2021-06-28 06:03:28 -0500

    Swapped scenario2d for vec2d

--- src/bb.ts
+++ src/bb.ts
@@ -1,14 +1,17 @@
 
-import { Vec2 } from "@repcomm/scenario2d";
+import { Vec2 } from "@repcomm/vec2d";
 
+/**Bounding box 2d
+ * 
+ */
 export class AABB {
   position: Vec2;
-  halfExtents: Vec2;
+  size: Vec2;
   velocity: Vec2;
 
   constructor() {
     this.position = new Vec2();
-    this.halfExtents = new Vec2();
+    this.size = new Vec2();
   }
   /**
    * https://www.gamedev.net/articles/programming/general-and-gameplay-programming/swept-aabb-collision-detection-and-response-r3084/
@@ -21,47 +24,47 @@ export class AABB {
     //b1 is moving
 
     // let invEntry: Vec2;
-    let xInvEntry, yInvEntry;
-    let xInvExit, yInvExit;
+    let xInvEntry: number;
+    let yInvEntry: number;
+    let xInvExit: number;
+    let yInvExit: number;
 
-    // find the distance between the objects on the near and far sides for both x and y 
+    // find the distance between the objects on the near and far sides for both x and y
+
     if (b1.velocity.x > 0.0) {
-      xInvEntry = b2.position.x - (b1.position.x + b1.halfExtents.w);
-      xInvExit = (b2.x + b2.w) - b1.x;
-    } else 
-{
-  xInvEntry = (b2.x + b2.w) - b1.x;
-  xInvExit = b2.x - (b1.x + b1.w);
-}
+      xInvEntry = b2.position.x - (b1.position.x + b1.size.x);
+      xInvExit = (b2.position.x + b2.size.x) - b1.position.x;
+    } else {
+      xInvEntry = (b2.position.x + b2.size.x) - b1.position.x;
+      xInvExit = b2.position.x - (b1.position.x + b1.size.x);
+    }
 
-if (b1.vy > 0.0f)
-{
-  yInvEntry = b2.y - (b1.y + b1.h);
-  yInvExit = (b2.y + b2.h) - b1.y;
-}
-else
-{
-  yInvEntry = (b2.y + b2.h) - b1.y;
-  yInvExit = b2.y - (b1.y + b1.h);
-}
+    if (b1.velocity.y > 0.0) {
+      yInvEntry = b2.position.y - (b1.position.y + b1.size.y);
+      yInvExit = (b2.position.y + b2.size.y) - b1.position.y;
+    }
+    else {
+      yInvEntry = (b2.position.y + b2.size.y) - b1.position.y;
+      yInvExit = b2.position.y - (b1.position.y + b1.size.y);
+    }
   }
   static extend(aabb: AABB, left: number, right: number, up: number, down: number) {
-  if (left !== 0) {
-    aabb.halfExtents.x += left / 2;
-    aabb.position.x -= left / 2;
+    if (left !== 0) {
+      aabb.size.x += left / 2;
+      aabb.position.x -= left / 2;
+    }
+    if (right !== 0) {
+      aabb.size.x += right / 2;
+      aabb.position.x += right / 2;
+    }
+    if (up !== 0) {
+      aabb.size.y += up / 2;
+      aabb.position.y -= up / 2;
+    }
+    if (down !== 0) {
+      aabb.size.y += down / 2;
+      aabb.position.y += down / 2;
+    }
   }
-  if (right !== 0) {
-    aabb.halfExtents.x += right / 2;
-    aabb.position.x += right / 2;
-  }
-  if (up !== 0) {
-    aabb.halfExtents.y += up / 2;
-    aabb.position.y -= up / 2;
-  }
-  if (down !== 0) {
-    aabb.halfExtents.y += down / 2;
-    aabb.position.y += down / 2;
-  }
-}
-  
+
 }
